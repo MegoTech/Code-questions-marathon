60a135557219d7d13acf62b05b612e77fe0b8e7b
moshe_h_m <mhm23811@gmail.com>
2023-03-28 14:10:15 +0300

    unresolved question for 28-3 ( the corrected )

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ from RemoveDuplicates import remove_duplicates
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    pased = 0
     nums = [1, 1, 2]
     k = remove_duplicates(nums)
     if k == 2 and nums[:k] == [1, 2]:
+        pased += 1
         print("Test 1 Passed")
     nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
     k = remove_duplicates(nums)
     if k == 5 and nums[:k] == [0, 1, 2, 3, 4]:
+        pased += 1
         print("Test 2 Passed")
-    print('Done! Good Job! Alufffffffffffffffff!!!!!!')
+    if pased == 2:
+        print('Done! Good Job! Alufffffffffffffffff!!!!!!')
+    elif pased == 1:
+        print('only one test passed, try again')
+    else:
+        print('No test passed, try again')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
