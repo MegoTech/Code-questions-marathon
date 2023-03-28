637bd758a81ed359f3d5dfb4b307db4a0d5366dd
moshe_h_m <mhm23811@gmail.com>
2023-03-29 01:25:45 +0300

    unresolved question for 29-3

---
+++ BuyAndSell.py
@@ -0,0 +1,14 @@
+#this quistion is for the 29/03/2023 daily coding problem
+#
+# You are given an array prices where prices[i] is the price of a given stock
+# on the ith day.
+#
+# You want to maximize your profit by choosing a single day to buy one stock
+# and choosing a different day in the future to sell that stock.
+#
+# Return the maximum profit you can achieve from this transaction.
+# If you cannot achieve any profit, return 0.
+
+def best_deal(prices: list) -> int:
+    # implement your code here
+    return 0
