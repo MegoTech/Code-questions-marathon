236cbf5957d3055d04028eab07b625b46870755c
moshe_h_m <mhm23811@gmail.com>
2023-05-11 15:41:41 +0300

    for weekly question

---
+++ Dictionary/charTimes.py
@@ -0,0 +1,15 @@
+# Write a code for a program that reads in a string and counts how many times each character
+# appears. Then, print out a list of tuples where each tuple contains a character and its corresponding
+# count. Starting from the most frequent letter to the least frequent one. For example:
+# Input: 'hello world';
+# Output:  [('l', 3), ('o', 2), ('h', 1), ('e', 1), (' ', 1), ('w', 1), ('r', 1), ('d', 1)]
+#
+# Write your code tied and clear on your Python and upload your solution to github. Do not forget to
+# pay attention to running-time issues and consider the additional following challenge: Try to solve
+# this problem with the least number of lines of code. To do it, consider using a lambda function to
+# define a custom sorting order for the list of tuples representing the character counts or Counter
+# class from the collections module.
+
+def count_char(string: str) -> list:
+    #implement your code here
+    return []
